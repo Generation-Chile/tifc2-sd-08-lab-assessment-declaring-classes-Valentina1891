9925ae8899e12d76de1c53923ea4056cfb03ca64
Valentina1891 <vvaldiviam@uft.edu>
2025-08-25 14:59:29 -0400

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,7 +1,22 @@
+// Ahora te han pedido incluir un método para subir de nivel, aumentando el número de nivel en uno.
+
+// Modifica la clase Player para que acepte un name y un level como dos argumentos separados.
+
+// Luego define un método compartido info() que imprima:
+
+// <name> has reached Level <level>!
+
+// Finalmente, define un segundo método compartido llamado levelUp() que incremente el nivel del jugador.
 export class Player {
-  constructor() {
-   
+  constructor(name,level) {
+   this.name =name;
+   this.level = level;
   }
-  
+  info(){ //Obtener información
+    console.log(`${this.name} has reached Level ${this.level}!`)
+  }
+  levelUp(){ //Subir nivel
+    this.level +=1;
+  }
   
 }
