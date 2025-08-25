5117a235af888cdd0ba226713f711978b73a0544
Valentina1891 <vvaldiviam@uft.edu>
2025-08-25 14:59:27 -0400

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,8 +1,20 @@
+// Ahora te han pedido incluir un método que imprima un mensaje en la consola anunciando una subida de nivel.
+
+// Modifica la clase Player para que acepte un name y un level como dos argumentos separados.
+
+// Luego define un método compartido llamado info() que imprima el siguiente string, reemplazando los dos valores:
+
+// // <name> has reached Level <level>!
+
+// Por ejemplo: un jugador llamado Tara en el nivel 6 debería imprimir en consola:
+"Tara has reached Level 6!"
 export class Player {
-  constructor() {
-  
+  constructor(name, level) {
+    this.name = name;
+    this.level = parseInt(level);
   }
-
-
+  info(){ //obtener información
+    console.log(`${this.name} has reached Level ${this.level}!`)
+  }
 
 }
